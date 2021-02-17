13eaee7597916429d03f0563ba0c8d3f15e38cf2
Xin Jin <xinjin1109@gmail.com>
2021-02-17 10:01:00 +0100

    add ellipse

--- ch1_introduction/hw1/lesson one/pca_data.py
+++ ch1_introduction/hw1/lesson one/pca_data.py
@@ -32,8 +32,19 @@ def PCA(data, correlation=False, sort=True):
 
 # generate data of normal distribution N(0,1) 
 center = [0.0, 0.0]
-data = np.random.multivariate_normal(center,np.array([[100.0,100.0],[0.0,20.0]]),10000)
-print(np.cov(data.T))
+cov = np.array([[100.0,100.0],[0.0,20.0]])
+data = np.random.multivariate_normal(center,cov,10000)
+
+# draw ellipse
+eigenvalues, eigenvectors = np.linalg.eig(cov)
+print("@@@",eigenvectors)
+sort = eigenvalues.argsort()[::-1]
+eigenvalues = eigenvalues[sort]
+eigenvectors = eigenvectors[:, sort]
+angle = - np.arccos(np.dot(eigenvectors[:,1],np.array([1,0]))) * 180 / np.pi
+print(angle)
+ellipse = Ellipse(center,eigenvalues[0], eigenvalues[1], angle,facecolor= 'yellow', alpha=0.3)
+
 data - np.mean(data, axis = 0)
 
 # draw data
@@ -41,16 +52,17 @@ fig = plt.figure()
 plt.scatter(data[:,0], data[:,1],color = [0,1,0])
 plt.axis('equal')
 ax = fig.add_subplot(111)
+ax.add_patch(ellipse)
 ax.set_title('Data')   
 plt.xlabel('X1') #设置X轴标签 
 plt.ylabel('X2') #设置Y轴标签  
 
 
-w,v = PCA(data,True)
+w,v = PCA(data,False)
 plt.scatter(center[0], center[1], marker = 'x', color = 'r', s = 30)
 
-tmp_p1 = center + v[:,0] * w[0] * 10
-tmp_p2 = center + v[:,1] * w[1] * 10
+tmp_p1 = center + v[:,0] * w[0] 
+tmp_p2 = center + v[:,1] * w[1] 
 
 x1, y1 = [center[0], tmp_p1[0]], [center[1], tmp_p1[1]]
 x2, y2 = [center[0], tmp_p2[0]], [center[1], tmp_p2[1]]
