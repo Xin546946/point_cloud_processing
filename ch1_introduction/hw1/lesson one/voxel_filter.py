2908e94e09a50c81a54dbda49e45f0c8d4b9c79b
Xin Jin <xinjin1109@gmail.com>
2021-02-17 18:40:32 +0100

    voxel filter bug version

--- ch1_introduction/hw1/lesson one/voxel_filter.py
+++ ch1_introduction/hw1/lesson one/voxel_filter.py
@@ -13,8 +13,38 @@ def voxel_filter(point_cloud, leaf_size):
     filtered_points = []
     # 作业3
     # 屏蔽开始
+    # compute the min or max of the point set {p1,p2,p3,...}
+    x_max, y_max, z_max = np.max(point_cloud,axis = 0)
+    x_min, y_min, z_min = np.min(point_cloud,axis = 0)
 
- 
+    # compute the dim of the voxel grid
+    Dx = np.round((x_max - x_min) / leaf_size) 
+    Dy = np.round((y_max - y_min) / leaf_size)
+    Dz = np.round((z_max - z_min) / leaf_size)
+    
+    # compute voxel idx for each point
+    h = list()
+    
+    for i in range(len(point_cloud)):
+        x,y,z = point_cloud[i]
+        hx = np.floor((x - x_min) / r)
+        hy = np.floor((y - y_min) / r)
+        hz = np.floor((z - z_min) / r) 
+        h.append(hx + hy * Dx + hz * Dx * Dy)
+    h = np.asarray(h)
+
+    h_sorted_idx = np.argsort(h)
+    h_sorted_pts = h[h_sorted_idx]
+
+    begin = 0
+    for i in range(len(h_sorted_idx) -1):
+        if h_sorted_idx[i] == h_sorted_idx[i + 1]:
+            continue
+        else:
+            point_idx = h_sorted_idx[begin, i + 1]
+            filtered_points.append(np.mean(point_cloud[point_idx], axis = 0))
+            begin = i
+
     # 屏蔽结束
 
     # 把点云格式改成array，并对外返回
@@ -30,7 +60,7 @@ def main():
     # point_cloud_pynt = PyntCloud.from_file(file_name)
 
     # 加载自己的点云文件
-    file_name = "/Users/renqian/Downloads/program/cloud_data/11.ply"
+    file_name = "/home/kit/point_cloud_processing/ch1_introduction/hw1/ply_data/airplane/test/1.ply"
     point_cloud_pynt = PyntCloud.from_file(file_name)
 
     # 转成open3d能识别的格式
