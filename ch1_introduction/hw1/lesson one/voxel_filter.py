f9d3d380c5b56cac0cd8bc215601edd9bf9b3ae7
Xin Jin <xinjin1109@gmail.com>
2021-02-19 21:34:53 +0100

    solve hash conflict

--- ch1_introduction/hw1/lesson one/voxel_filter.py
+++ ch1_introduction/hw1/lesson one/voxel_filter.py
@@ -3,13 +3,23 @@
 import open3d as o3d 
 import os
 import numpy as np
+import pandas as pd
 from pyntcloud import PyntCloud
+from functools import cmp_to_key
+
+
+def hash_table_conflict(h_lhs, h_rhs):
+    if h_lhs[0] == h_rhs[0] and h_lhs[1] == h_rhs[1] and h_lhs[2] == h_rhs[2]:
+        return False
+    else:
+        return True
+
 
 # 功能：对点云进行voxel滤波
 # 输入：
 #     point_cloud：输入点云
 #     leaf_size: voxel尺寸
-def voxel_filter(point_cloud, leaf_size):
+def voxel_filter(point_cloud, leaf_size, random_downsampling = False):
     point_cloud = np.asarray(point_cloud)
     print("Number of point clouds is", len(point_cloud))
     filtered_points = []
@@ -23,7 +33,7 @@ def voxel_filter(point_cloud, leaf_size):
     Dx = (x_max - x_min) // leaf_size
     Dy = (y_max - y_min) // leaf_size
     Dz = (z_max - z_min) // leaf_size
-         
+    container_size = Dx * Dy * Dz
     # compute voxel idx for each point
     h = list()
     
@@ -31,21 +41,37 @@ def voxel_filter(point_cloud, leaf_size):
         x,y,z = point_cloud[i]
         hx = np.floor((x - x_min) / leaf_size)
         hy = np.floor((y - y_min) / leaf_size)
-        hz = np.floor((z - z_min) / leaf_size) 
-        h.append(int(hx + hy * Dx + hz * Dx * Dy))
+        hz = np.floor((z - z_min) / leaf_size)
+        hash_table = int(hx + hy * Dx + hz * Dx * Dy) % container_size
+        h.append(np.asarray([hx, hy, hz, hash_table]))
+        # h.append(hash_table)
+    # h_sorted = np.asarray(sorted(h, key = cmp_to_key(lambda lhs, rhs : lhs[3] - rhs[3]))) # 点在第几个voxel
     
-    h_sorted = sorted(h) # 点在第几个voxel
-    h_sorted_idx = np.argsort(h)
+    h = np.asarray(h)
+    # current_h_sorted_idx = np.lexsort((current_h[:,0], current_h[:,1]))
+    h_sorted_idx =np.lexsort((h[:,-1], h[:,0], h[:,1])) # 按照h的最后一列排序,再按照第一列排序，再按照第二列排序
+    h_sorted = h[h_sorted_idx]
+    # h_sorted_idx = np.argsort(h)
     current_voxel = list()
-    
+    current_h = list()
+
     for i in range(point_cloud.shape[0] -1):
-        if h_sorted[i] == h_sorted[i + 1]:
+        if h_sorted[i, -1] == h_sorted[i + 1, -1] and not hash_table_conflict(h_sorted[i,0:3], h_sorted[i+1, 0:3]):
             current_voxel.append(point_cloud[h_sorted_idx[i]])
+            # current_h.append(h_sorted[i, 0:3])
         else:
-            # point_idx = h_sorted_idx[begin: i + 1]
             current_voxel.append(point_cloud[h_sorted_idx[i]])
-            filtered_points.append(np.mean(np.array(current_voxel), axis = 0))
+            # current_h.append(h_sorted[i, 0:3])
+            
+            if(random_downsampling == False):
+                filtered_points.append(np.mean(np.array(current_voxel), axis = 0))
+            else:
+                # random_idx = np.random.choice(range(len(current_voxel)), 1)
+                random_idx = np.random.randint(len(current_voxel), size = 1)
+                filtered_points.append(current_voxel[int(random_idx)])
             current_voxel.clear()
+            # current_h.clear()
+
     # 屏蔽结束
 
     # 把点云格式改成array，并对外返回
@@ -67,11 +93,11 @@ def main():
 
     # 转成open3d能识别的格式
     point_cloud_o3d = point_cloud_pynt.to_instance("open3d", mesh=False)
-    o3d.visualization.draw_geometries([point_cloud_o3d]) # 显示原始点云
+    # o3d.visualization.draw_geometries([point_cloud_o3d], "Orignal point cloud") # 显示原始点云
 
     # 调用voxel滤波函数，实现滤波
 
-    filtered_cloud = voxel_filter(point_cloud_pynt.points, 0.1)
+    filtered_cloud = voxel_filter(point_cloud_pynt.points, 100.0, True)
     point_cloud_o3d.points = o3d.utility.Vector3dVector(filtered_cloud)
     # 显示滤波后的点云
     o3d.visualization.draw_geometries([point_cloud_o3d],"Voxel grid downsampling")
