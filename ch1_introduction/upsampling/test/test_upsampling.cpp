d07308a0e4884ab87e0f1da6408045caf51a6960
Xin Jin <xinjin1109@gmail.com>
2021-02-28 22:26:35 +0100

    debug upsampling

--- ch1_introduction/upsampling/test/test_upsampling.cpp
+++ ch1_introduction/upsampling/test/test_upsampling.cpp
@@ -11,15 +11,15 @@
 // weight???
 int main(int argc, char **argv) {
   cv::Mat img = read_img(argv[1], cv::IMREAD_GRAYSCALE);
+
   img.convertTo(img, CV_64FC1);
   cv::Mat sparse_depth = read_img(argv[2], cv::IMREAD_GRAYSCALE);
   sparse_depth.convertTo(sparse_depth, CV_64FC1);
   //   sparse_depth.convertTo(sparse_depth, CV_64FC1);
 
   cv::Mat upsapl_img =
-      apply_bilateral_filter_for_upsampling(sparse_depth, 7, 5.0, 5.0);
-  // upsapl_img = apply_bilateral_filter_for_upsampling(upsapl_img, 5,
-  // 5.0, 5.0);
+      apply_bilateral_filter_for_upsampling(sparse_depth, 11, 7.0, 5.0);
+
   //   cv::Mat vis_upsamp = apply_jetmap(upsapl_img);
   //   cv::Mat vis_depth = apply_jetmap(sparse_depth);
   img.convertTo(img, CV_8UC1);
@@ -29,7 +29,8 @@ int main(int argc, char **argv) {
 
   std::cout << img.type() << " " << jet_map_sparse_depth.type() << " " << '\n';
   cv::Mat vis_temp;
-  cv::vconcat(img, jet_map_sparse_depth, vis_temp);
+  cv::vconcat(read_img(argv[1], cv::IMREAD_COLOR), jet_map_sparse_depth,
+              vis_temp);
   cv::Mat jet_map_depth = apply_jetmap(upsapl_img);
   //<< jet_map_depth.type() << '\n';
   cv::Mat vis;
