475e1bcaf25466d3bab85ad4999b58a64fe0f067
Xin Jin <xinjin1109@gmail.com>
2021-03-02 20:50:58 +0100

    rename imgs

---
+++ ch1_introduction/upsampling/test/test_upsamp_all_imgs.cpp
@@ -0,0 +1,15 @@
+#include "bilateral_filter.h"
+#include "display.h"
+#include "opencv_utils.h"
+#include "tictoc.h"
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+int main(int argc, char **argv) {
+  std::vector<cv::Mat> imgs;
+  for (int id = 1; id <)
+
+    return 0;
+}
