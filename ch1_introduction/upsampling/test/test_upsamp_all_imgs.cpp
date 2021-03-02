5e27b72943fdc723f31bba854fc60ebbf7b4a5f1
Xin Jin <xinjin1109@gmail.com>
2021-03-02 21:20:30 +0100

    delete some imgs

--- ch1_introduction/upsampling/test/test_upsamp_all_imgs.cpp
+++ ch1_introduction/upsampling/test/test_upsamp_all_imgs.cpp
@@ -8,8 +8,26 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 int main(int argc, char **argv) {
-  std::vector<cv::Mat> imgs;
-  for (int id = 1; id <)
+  std::vector<cv::Mat> depths;
 
-    return 0;
+  for (int id = 1; id < 101; id++) {
+    cv::Mat depth =
+        read_img(argv[1] + std::to_string(id) + ".png", cv::IMREAD_GRAYSCALE);
+    depths.push_back(depth);
+  }
+
+  cv::Mat upsampl_img;
+
+  for (int id = 1; id < 101; id++) {
+
+    upsampl_img =
+        apply_bilateral_filter_for_upsampling(depths[id], 11, 7.0, 5.0);
+    cv::imwrite(
+        "../datas/depth_selection/val_selection_cropped/prediction_depth" +
+            std::to_string(id) + ".png",
+        upsampl_img);
+    std::cout << "Finish processing the " << id << "-th depth image." << '\n';
+  }
+
+  return 0;
 }
