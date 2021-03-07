42a66d653fff4658f1d5181a597b8aee3a4bcff0
Xin Jin <xinjin1109@gmail.com>
2021-03-07 10:34:31 +0100

    timing for upsampling 0.4

--- ch1_introduction/upsampling/test/test_upsamp_all_imgs.cpp
+++ ch1_introduction/upsampling/test/test_upsamp_all_imgs.cpp
@@ -21,23 +21,26 @@ int main(int argc, char **argv) {
   std::vector<cv::Mat> upsampl_imgs;
 
   for (int id = 1; id < 101; id++) {
-
+    tictoc::tic();
     upsampl_img =
         apply_bilateral_filter_for_upsampling(depths[id - 1], 11, 7.0, 5.0);
+    std::cout << std::to_string(id) << "-th frame costs " << tictoc::toc() / 1e6
+              << "s" << '\n';
     cv::Mat jet_map_depth = apply_jetmap(upsampl_img);
     cv::Mat ground_truth =
         read_img(argv[2] + std::to_string(id) + ".png", cv::IMREAD_GRAYSCALE);
     cv::Mat jet_map_ground_truth = apply_jetmap(ground_truth);
     cv::Mat vis;
     cv::vconcat(jet_map_depth, jet_map_ground_truth, vis);
-    cv::imshow("depth_map", vis);
+    cv::imshow("up: my upsampling; down: groundtruth", vis);
     cv::waitKey(1);
 
     cv::imwrite(
         "../datas/depth_selection/val_selection_cropped/prediction_depth/" +
             std::to_string(id) + ".png",
         upsampl_img);
-    std::cout << "Finish processing the " << id << "-th depth image." << '\n';
+    // std::cout << "Finish processing the " << id << "-th depth image." <<
+    // '\n';
   }
 
   return 0;
