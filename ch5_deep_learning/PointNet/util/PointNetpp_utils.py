a53ec07d0e5b74971170b6ce97f8f0c20dad3235
Xin Jin <xinjin1109@gmail.com>
2021-04-06 23:28:02 +0200

    farthest_point_sample need to be implemented

--- ch5_deep_learning/PointNet/util/PointNetpp_utils.py
+++ ch5_deep_learning/PointNet/util/PointNetpp_utils.py
@@ -5,11 +5,53 @@ import torch.nn.functional as F
 
 from time import time
 
-# class PointNetModule(nn.Module):
-#     def __init_(self, points, )
+def farthest_point_sample_(xyz, npoint):
+    """
+    
+    Input:
+        xyz: pointcloud data, [B, N, 3]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [B, npoint]
+    """
+    device = xyz.device
+    B, N, C = xyz.shape
+    centroids = torch.zeros(B, npoint, dtype=torch.long).to(device)
+    distance = torch.ones(B, N).to(device) * 1e10
+    farthest = torch.randint(0, N, (B,), dtype=torch.long).to(device)
+    batch_indices = torch.arange(B, dtype=torch.long).to(device)
+    import pdb; pdb.set_trace()
+
+    for i in range(npoint):
+        centroids[:, i] = farthest
+        centroid = xyz[batch_indices, farthest, :].view(B, 1, 3)
+        dist = torch.sum((xyz - centroid) ** 2, -1)
+        mask = dist < distance
+        distance[mask] = dist[mask]
+        farthest = torch.max(distance, -1)[1]
+    return centroids
 
 
-class SetAbstraction(nn.Module):
+def farthest_point_sample(points_xyz, num_samples):
+    """
+    Tutorial: https://blog.csdn.net/dsoftware/article/details/107184116
+    Input: 
+        point_xyz [batch_size, num_points, channels]
+        num_samples: the number of samples of FPS
+    Output:
+        samples_xyz [batch_size, num_samples, channels] 
+    """
+    device = points_xyz.device
+    batch_size, num_points, channels = points_xyz.shape
+
+    samples_xyz = torch.zeros(batch_size, num_samples, dtype=torch.long).to(device) # samples should have size discussed above 
+    distances_container = torch.ones(batch_size, num_points).to(device) * 1e10 # distance matrix for one point and all points(batch_size * num_points)
+    
+    for i in range(num_samples):
+        samples_xyz[:, i] = torch
+
+
+class SetAbstraction(torch.nn.Module):
     # 1. FPS sampling
     # 2. group
     # 3. PointNet
@@ -43,9 +85,13 @@ class SetAbstraction(nn.Module):
         if point_features is not None: # point features is normal vector in this case
             point_features = points.permute(0, 2, 1)
         
-        if self.group_all
+        # if self.group_all
 
         # 2. group
         
         # 3. PointNet
-        
+
+if __name__ == '__main__':
+    points_xyz = torch.randn(5,100,3)
+    num_samples = 50
+    samples_xyz = farthest_point_sample_(points_xyz, num_samples)
