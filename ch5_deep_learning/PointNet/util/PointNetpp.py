cd18035d298fa960b2ae4a05e499d38795f8c5e9
Xin Jin <xinjin1109@gmail.com>
2021-04-11 20:36:05 +0200

    PointNet++

--- ch5_deep_learning/PointNet/util/PointNetpp.py
+++ ch5_deep_learning/PointNet/util/PointNetpp.py
@@ -13,12 +13,9 @@ class PointNetpp(nn.Module):
         self.num_class = num_class
         in_channel = 6 if normal_channel else 3
         self.normal_channel = normal_channel
-        self.set_abstraction1 = SetAbstraction(num_points = 512, radius = 0.2, num_sample = 32, 
-                                            in_channel = in_channel, mlp = [64, 64, 128], group_all = False)
-        self.set_abstraction2 = SetAbstraction(num_points = 128, radius = 0.4, num_sample = 64, 
-                                            in_channel = 128 + 3, mlp = [128, 128, 256], group_all = False)
-        self.set_abstraction1 = SetAbstraction(num_points = 512, radius = 0.2, num_sample = 32, 
-                                            in_channel = 256 + 3, mlp = [256, 512, 1024], group_all = True)
+        self.sa1 = PointNetSetAbstraction(npoint=512, radius=0.2, nsample=32, in_channel=in_channel, mlp=[64, 64, 128], group_all=False)
+        self.sa2 = PointNetSetAbstraction(npoint=128, radius=0.4, nsample=64, in_channel=128 + 3, mlp=[128, 128, 256], group_all=False)
+        self.sa3 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, in_channel=256 + 3, mlp=[256, 512, 1024], group_all=True)
         self.fc1 = nn.Linear(1024, 512)
         self.bn1 = nn.BatchNorm1d(512)
         self.dropout1 = nn.Dropout(0.4)
@@ -27,26 +24,29 @@ class PointNetpp(nn.Module):
         self.dropout2 = nn.Dropout(0.4)
         self.fc3 = nn.Linear(256, self.num_class)
 
-        def forward(self, points): # points [batch_size, dim, num_points]
+        def forward(self, x): # points [batch_size, dim, num_points]
             batch_size = points.shape[0]
-            if self.normal_channel:
-                point_norm = points[:, 3:, :] # get normal vector of points (from the 3nd element of 2nd dim)
-                point_xyz = points[:, :3, :] # get point position [x,y,z]
-            else:
-                point_norm = None
-            
+        
             # extract local features: 
-            point_xyz, point_features = self.set_abstraction1(point_xyz, point_norm)
-            point_xyz, point_feature = self.set_abstraction2(point_xyz, point_features)
-            point_xyz, point_features = self.set_abstraction3(point_xyz, point_features)
+            B, _, _ = xyz.shape
+            if self.normal_channel:
+                norm = xyz[:, 3:, :]
+                xyz = xyz[:, :3, :]
+            else:
+                norm = None
+
+            l1_xyz, l1_points = self.sa1(xyz, norm)
+            l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
+            l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
+            
 
             # resize features to b * num for mlp
-            x = point_features_3.view(batch_size, 1024)
+            x = l3_points.view(B, 1024)
 
             # mlp with drop out
             x = self.dropout1(F.relu(self.bn1(self.fc1(x))))
             x = self.dropout2(F.relu(self.bn2(self.fc2(x))))
             x = self.fc3(x)
 
-            return x, point_feature_3
+            return x, l3_points
 
--- ch5_deep_learning/PointNet/util/PointNetpp_utils.py
+++ ch5_deep_learning/PointNet/util/PointNetpp_utils.py
@@ -5,9 +5,49 @@ import torch.nn.functional as F
 
 from time import time
 
-def farthest_point_sample_(xyz, npoint):
+def square_distance(src, dst):
     """
-    
+    Calculate Euclid distance between each two points.
+    src^T * dst = xn * xm + yn * ym + zn * zm；
+    sum(src^2, dim=-1) = xn*xn + yn*yn + zn*zn;
+    sum(dst^2, dim=-1) = xm*xm + ym*ym + zm*zm;
+    dist = (xn - xm)^2 + (yn - ym)^2 + (zn - zm)^2
+         = sum(src**2,dim=-1)+sum(dst**2,dim=-1)-2*src^T*dst
+    Input:
+        src: source points, [B, N, C]
+        dst: target points, [B, M, C]
+    Output:
+        dist: per-point square distance, [B, N, M]
+    """
+    B, N, _ = src.shape
+    _, M, _ = dst.shape
+    dist = -2 * torch.matmul(src, dst.permute(0, 2, 1))
+    dist += torch.sum(src ** 2, -1).view(B, N, 1)
+    dist += torch.sum(dst ** 2, -1).view(B, 1, M)
+    return dist
+
+
+def index_points(points, idx):
+    """
+    Input:
+        points: input points data, [B, N, C]
+        idx: sample index data, [B, S]
+    Return:
+        new_points:, indexed points data, [B, S, C]
+    """
+    device = points.device
+    B = points.shape[0]
+    view_shape = list(idx.shape)
+    view_shape[1:] = [1] * (len(view_shape) - 1)
+    repeat_shape = list(idx.shape)
+    repeat_shape[0] = 1
+    batch_indices = torch.arange(B, dtype=torch.long).to(device).view(view_shape).repeat(repeat_shape)
+    new_points = points[batch_indices, idx, :]
+    return new_points
+
+
+def farthest_point_sample(xyz, npoint):
+    """
     Input:
         xyz: pointcloud data, [B, N, 3]
         npoint: number of samples
@@ -20,8 +60,6 @@ def farthest_point_sample_(xyz, npoint):
     distance = torch.ones(B, N).to(device) * 1e10
     farthest = torch.randint(0, N, (B,), dtype=torch.long).to(device)
     batch_indices = torch.arange(B, dtype=torch.long).to(device)
-    import pdb; pdb.set_trace()
-
     for i in range(npoint):
         centroids[:, i] = farthest
         centroid = xyz[batch_indices, farthest, :].view(B, 1, 3)
@@ -32,66 +70,125 @@ def farthest_point_sample_(xyz, npoint):
     return centroids
 
 
-def farthest_point_sample(points_xyz, num_samples):
+def query_ball_point(radius, nsample, xyz, new_xyz):
     """
-    Tutorial: https://blog.csdn.net/dsoftware/article/details/107184116
-    Input: 
-        point_xyz [batch_size, num_points, channels]
-        num_samples: the number of samples of FPS
-    Output:
-        samples_xyz [batch_size, num_samples, channels] 
+    Input:
+        radius: local region radius
+        nsample: max sample number in local region
+        xyz: all points, [B, N, 3]
+        new_xyz: query points, [B, S, 3]
+    Return:
+        group_idx: grouped points index, [B, S, nsample]
     """
-    device = points_xyz.device
-    batch_size, num_points, channels = points_xyz.shape
+    device = xyz.device
+    B, N, C = xyz.shape
+    _, S, _ = new_xyz.shape
+    group_idx = torch.arange(N, dtype=torch.long).to(device).view(1, 1, N).repeat([B, S, 1])
+    sqrdists = square_distance(new_xyz, xyz)
+    group_idx[sqrdists > radius ** 2] = N
+    group_idx = group_idx.sort(dim=-1)[0][:, :, :nsample]
+    group_first = group_idx[:, :, 0].view(B, S, 1).repeat([1, 1, nsample])
+    mask = group_idx == N
+    group_idx[mask] = group_first[mask]
+    return group_idx
 
-    samples_xyz = torch.zeros(batch_size, num_samples, dtype=torch.long).to(device) # samples should have size discussed above 
-    distances_container = torch.ones(batch_size, num_points).to(device) * 1e10 # distance matrix for one point and all points(batch_size * num_points)
-    
-    for i in range(num_samples):
-        samples_xyz[:, i] = torch
+
+def sample_and_group(npoint, radius, nsample, xyz, points, returnfps=False):
+    """
+    Input:
+        npoint:
+        radius:
+        nsample:
+        xyz: input points position data, [B, N, 3]
+        points: input points data, [B, N, D]
+    Return:
+        new_xyz: sampled points position data, [B, npoint, nsample, 3]
+        new_points: sampled points data, [B, npoint, nsample, 3+D]
+    """
+    B, N, C = xyz.shape
+    S = npoint
+    fps_idx = farthest_point_sample(xyz, npoint) # [B, npoint, C]
+    new_xyz = index_points(xyz, fps_idx)
+    idx = query_ball_point(radius, nsample, xyz, new_xyz)
+    grouped_xyz = index_points(xyz, idx) # [B, npoint, nsample, C]
+    grouped_xyz_norm = grouped_xyz - new_xyz.view(B, S, 1, C)
+
+    if points is not None:
+        grouped_points = index_points(points, idx)
+        new_points = torch.cat([grouped_xyz_norm, grouped_points], dim=-1) # [B, npoint, nsample, C+D]
+    else:
+        new_points = grouped_xyz_norm
+    if returnfps:
+        return new_xyz, new_points, grouped_xyz, fps_idx
+    else:
+        return new_xyz, new_points
 
 
-class SetAbstraction(torch.nn.Module):
-    # 1. FPS sampling
-    # 2. group
-    # 3. PointNet
-    def __init__(self, num_points, radius, num_samples, in_channel, mlp, group_all):
-        super(SetAbstraction, self).__init__()
-        self.num_points = num_points
+def sample_and_group_all(xyz, points):
+    """
+    Input:
+        xyz: input points position data, [B, N, 3]
+        points: input points data, [B, N, D]
+    Return:
+        new_xyz: sampled points position data, [B, 1, 3]
+        new_points: sampled points data, [B, 1, N, 3+D]
+    """
+    device = xyz.device
+    B, N, C = xyz.shape
+    new_xyz = torch.zeros(B, 1, C).to(device)
+    grouped_xyz = xyz.view(B, 1, N, C)
+    if points is not None:
+        new_points = torch.cat([grouped_xyz, points.view(B, 1, N, -1)], dim=-1)
+    else:
+        new_points = grouped_xyz
+    return new_xyz, new_points
+
+
+class PointNetSetAbstraction(torch.nn.Module):
+    def __init__(self, npoint, radius, nsample, in_channel, mlp, group_all):
+        super(PointNetSetAbstraction, self).__init__()
+        self.npoint = npoint
         self.radius = radius
-        self.num_samples = num_samples
+        self.nsample = nsample
         self.mlp_convs = nn.ModuleList()
         self.mlp_bns = nn.ModuleList()
-
         last_channel = in_channel
         for out_channel in mlp:
             self.mlp_convs.append(nn.Conv2d(last_channel, out_channel, 1))
             self.mlp_bns.append(nn.BatchNorm2d(out_channel))
             last_channel = out_channel
-        
-        self.group_all = group_all # TODO what is the group all ???????
-    
-    def forward(self, point_xyz, point_features):
+        self.group_all = group_all
+
+    def forward(self, xyz, points):
         """
         Input:
-            point_xyz: input points feature in euclidean space [batch_size, channel(3), num_points]
-            point_features: input points feature: [batch_size, dim, num_points] # except x,y,z posiiton
-        Output:
-            new_xyz: resampled points feature in euclidean space [batch_size, channel(3), num_points_resampled]
-            new_points: points features: [batch_size, dim', num_points_resampled]
+            xyz: input points position data, [B, C, N]
+            points: input points data, [B, D, N]
+        Return:
+            new_xyz: sampled points position data, [B, C, S]
+            new_points_concat: sample points feature data, [B, D', S]
         """
-        # 1. sampling
-        point_xyz = point_xyz.permute(0, 2, 1)
-        if point_features is not None: # point features is normal vector in this case
-            point_features = points.permute(0, 2, 1)
-        
-        # if self.group_all
+        xyz = xyz.permute(0, 2, 1)
+        if points is not None:
+            points = points.permute(0, 2, 1)
 
-        # 2. group
-        
-        # 3. PointNet
+        if self.group_all:
+            new_xyz, new_points = sample_and_group_all(xyz, points)
+        else:
+            new_xyz, new_points = sample_and_group(self.npoint, self.radius, self.nsample, xyz, points)
+        # new_xyz: sampled points position data, [B, npoint, C]
+        # new_points: sampled points data, [B, npoint, nsample, C+D]
+        new_points = new_points.permute(0, 3, 2, 1) # [B, C+D, nsample,npoint]
+        for i, conv in enumerate(self.mlp_convs):
+            bn = self.mlp_bns[i]
+            new_points =  F.relu(bn(conv(new_points)))
+
+        new_points = torch.max(new_points, 2)[0]
+        new_xyz = new_xyz.permute(0, 2, 1)
+        return new_xyz, new_points
 
 if __name__ == '__main__':
     points_xyz = torch.randn(5,100,3)
     num_samples = 50
-    samples_xyz = farthest_point_sample_(points_xyz, num_samples)
+    samples_xyz = farthest_point_sample(points_xyz, num_samples)
+    
