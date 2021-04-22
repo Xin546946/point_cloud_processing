a3a7f1fcdebe15e062dc1b024c57b8c4d03b94dd
Xin Jin <xinjin1109@gmail.com>
2021-04-22 22:09:35 +0200

    add iss header

--- ch7_feature_detection/src/iss.h
+++ ch7_feature_detection/src/iss.h
@@ -1,1 +1,26 @@
 #pragma once
+#include <vector>
+#include "Eigen/Eigenvalues"
+class ISSKeypoints{
+    public:
+    ISSKeypoints();
+    void use_weighted_conv_matrix(bool should_use_weighted_conv_matrix);
+    void set_input_cloud(std::vector<std::vector<float>>& point_cloud);
+    void set_local_radius(float radius);
+    void set_non_max_radius(float radius);
+    void set_threshold(float g21, float g32);
+    void compute(std::vector<std::vector<float>>& key_points);
+
+    private:
+    Eigen::Vector3f get_eigen_values(size_t i);
+
+    bool use_weighted_con_matrix_;
+    float local_radius_;
+    float non_max_radius_;
+    float gama21_;
+    float gama32_;
+    int min_neighbors_;
+    std::vector<std::vector<int>>& rnn_idx_;
+    std::vector<std::vector<float>> point_cloud_;
+
+};
