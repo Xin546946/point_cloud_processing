ef119c20f2b0b6b0ef8f871f5e88f90d3799c14d
Xin Jin <xinjin1109@gmail.com>
2021-04-22 23:06:23 +0200

    update

--- ch7_feature_detection/src/iss.cpp
+++ ch7_feature_detection/src/iss.cpp
@@ -1,1 +1,29 @@
 #include "iss.h"
+
+void ISSKeypoints::use_weighted_conv_matrix(
+    bool should_use_weighted_conv_matrix) {
+  this->use_weighted_conv_matrix_ = should_use_weighted_conv_matrix;
+}
+
+void ISSKeypoints::set_input_cloud(
+    std::vector<std::vector<float>> &point_cloud) {
+  this->point_cloud_ = point_cloud;
+}
+
+void ISSKeypoints::set_local_radius(float radius) {
+  this->local_radius_ = radius;
+}
+
+void ISSKeypoints::set_non_max_radius(float radius) {
+  this->non_max_radius_ = radius;
+}
+
+void ISSKeypoints::set_threshold(float g21, float g32) {
+  this->gama21_ = g21;
+  this->gama32_ = g32;
+}
+
+void ISSKeypoints::set_min_neighbors(int min_neighbor) {
+  this->min_neighbors_ = min_neighbor;
+}
+void compute(std::vector<std::vector<float>> &key_points) {}
--- ch7_feature_detection/src/iss.h
+++ ch7_feature_detection/src/iss.h
@@ -9,12 +9,13 @@ class ISSKeypoints{
     void set_local_radius(float radius);
     void set_non_max_radius(float radius);
     void set_threshold(float g21, float g32);
+    void set_min_neighbors(int min_neighbor);
     void compute(std::vector<std::vector<float>>& key_points);
 
     private:
     Eigen::Vector3f get_eigen_values(size_t i);
 
-    bool use_weighted_con_matrix_;
+    bool use_weighted_conv_matrix_;
     float local_radius_;
     float non_max_radius_;
     float gama21_;
